4a44ab98e583bbfab89b2fcb45cf73694f2e8f88
Nathaniel Adair <nwadair@students.hamilton.edu>
2019-06-04 16:34:05 -0400

    minor changes to previous methods

--- Find.py
+++ Find.py
@@ -3,15 +3,16 @@
 
 class Find:
 	def findTerm(self, name):
-		"""returns the term named name or None if one DNE"""
-		assert isinstance(name, str)
-		return self.terms.get(name)
+		"""returns the term named name or name if it is a term and None otherwise"""
+		if isinstance(name, Term):
+			return name
+		if isinstance(name, str):
+			return self.terms.get(name)
+		return None
 
 	def findTerms(self, names):
 		"""returns all terms specified by a valid name in names and the empty set if none exist"""
 		assert isinstance(names, list)
-		assert all(map((lambda n: isinstance(n, str)), names))
-
 		return set(reduce((lambda a,b: union(a, b)),
 						filter((lambda s: s not is None),
 							map((lambda n: self.findTerm(n)), names))))
