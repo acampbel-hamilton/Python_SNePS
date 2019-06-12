9040a2ee4c36df08f3d403b7cf0a8830eb6c3062
Nathaniel Adair <nwadair@gemini.cs.hamilton.edu>
2019-06-12 15:35:46 -0400

    extraneous attributes from SNePS3 stripped from objects

--- SyntacticTypes.py
+++ SyntacticTypes.py
@@ -5,13 +5,8 @@ import re
 
 class Term:
 	"""root of the syntactic type class hierarchy"""
-	def __init__(self, name, activation_value=0, fired=None,
-	 recorded_firing=None, activation_marker=None, up_cableset={}):
+	def __init__(self, name, up_cableset={}):
 		self.name = name
-		self.activation_value = activation_value
-		self.fired = fired
-		self.recorded_firing = recorded_firing
-		self.activation_marker = activation_marker
 		self.up_cableset = up_cableset #maps slot names to sets of term names
 
 #consider replacing the following 3 functions with a dictionary which
@@ -47,22 +42,18 @@ class Base(Atom):
 
 class Variable(Atom):
 	"""a variable term ranging over a restricted domain"""
-	def __init__(self, name, activation_value=0, fired=None,
-	 recorded_firing=None, activation_marker=None, up_cableset={},
+	def __init__(self, name, up_cableset={},
 	 restriction_set=set(), var_label=None):
-		Atom.__init__(self, name, activation_value, fired,
-		 recorded_firing, activation_marker, up_cableset)
+		Atom.__init__(self, name, up_cableset)
 		self.restriction_set = restriction_set
 		self.var_label = var_label
 
 class Indefinite(Variable):
 	"""an indefinite object"""
 	ind_counter = 0
-	def __init__(self, name, activation_value=0, fired=None,
-	 recorded_firing=None, activation_marker=None, up_cableset={},
+	def __init__(self, name, up_cableset={},
 	 restriction_set=set(), var_label=None, dependencies=set()):
-		Variable.__init__(self, name, activation_value, fired, recorded_firing,
-		 activation_marker, up_cableset, restriction_set, var_label)
+		Variable.__init__(self, name, up_cableset, restriction_set, var_label)
 		self.dependencies = dependencies
 
 class Arbitrary(Variable):
@@ -73,23 +64,16 @@ class Molecular(Term):
 	"""a functional term with zero or more arguments
 	equivalently a frame with slots and fillers"""
 	wftcounter = 0
-	def __init__(self, name, caseframe, down_cableset, down_weights,
-	 activation_value=0, fired=None, recorded_firing=None,
-	  activation_marker=None, up_cableset={}):
-		Term.__init__(self, name, activation_value, fired,
-		 recorded_firing, activation_marker, up_cableset)
+	def __init__(self, name, caseframe, down_cableset, up_cableset={}):
+		Term.__init__(self, name, up_cableset)
 		self.caseframe = caseframe
 		self.down_cableset = down_cableset #map from slot names to sets of term names
 		self.down_weights = down_weights
 
 class Param2Op(Molecular):
 	"""the andor or thresh of some proposition(s)"""
-	def __init__(self, name, caseframe, down_cableset, down_weights, min, max,
-	 activation_value=0, fired=None, recorded_firing=None,
-	 activation_marker=None, up_cableset={}):
-		Molecular.__init__(self, name, caseframe, down_cableset, down_weights,
-		 activation_value, fired, recorded_firing, activation_marker,
-		 up_cableset)
+	def __init__(self, name, caseframe, down_cableset, min, max, up_cableset={}):
+		Molecular.__init__(self, name, caseframe, down_cableset, up_cableset)
 		self.min = min
 		self.max = max
 
@@ -116,12 +100,8 @@ class Equivalence(Thresh):
 
 class NumericalEntailment(Molecular):
 	"""a numerical entailment"""
-	def __init__(self, name, caseframe, down_cableset, down_weights, min,
-	 activation_value=0, fired=None, recorded_firing=None,
-	 activation_marker=None, up_cableset={}):
-		Molecular.__init__(self, name, caseframe, down_cableset, down_weights,
-		 activation_value, fired, recorded_firing, activation_marker,
-		 up_cableset)
+	def __init__(self, name, caseframe, down_cableset, min, up_cableset={}):
+		Molecular.__init__(self, name, caseframe, down_cableset, up_cableset)
 		self.min = min
 
 class OrEntailment(NumericalEntailment):
