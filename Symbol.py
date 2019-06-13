3532c60163238377d5cf193de86af0db4c1385d9
Nathaniel Adair <nwadair@gemini.cs.hamilton.edu>
2019-06-13 12:58:55 -0400

    creation of symbol objects

---
+++ Symbol.py
@@ -0,0 +1,11 @@
+#PySNePS Symbols
+
+class Symbol(str):
+	"""a representation of symbols"""
+	pass
+
+def Sym(s, sym_tab={}):
+	"""return a symbol of the given string s if Symbol(s) DNE in sym_tab"""
+	if s not in sym_tab:
+		sym_tab[s] = Symbol(s)
+	return sym_tab[s]
