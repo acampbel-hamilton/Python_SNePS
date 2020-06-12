65de174ba55f650367fc1ffb6118284af375d3a8
swiseman <swiseman@hamilton.edu>
2020-06-12 15:47:06 -0400

    Began to implement filler methods on frames

--- Caseframe.py
+++ Caseframe.py
@@ -1,5 +1,5 @@
 class CaseFrame:
-	def __init__(self, name, docstring="", sem_type, slots=[]):
+	def __init__(self, name, sem_type, docstring="", slots=[]):
         self.name = name
         self.docstring = docstring
         self.sem_type = type
@@ -10,21 +10,22 @@ class Frame:
         self.caseframe = caseframe
 		self.fillers = fillers
 
+		if len(self.fillers) != len(self.caseframe.slots):
+			raise Exception("Wrong number of fillers")
+
+		fill_slots()
+
+	def fill_slots(self):
 		# Check fillers correspond to slots
 		# Fillers are entered as a list of lists:
 		#     - Each list correspond to one slot
 		#     - Remember one slot might have multiple fillers
-		if len(self.fillers) != len(self.caseframe.slots):
-			raise Exception("Wrong number of fillers")
 
-		for i in range(0, len(self.fillers) - 1):
+		for i in range(len(self.fillers) - 1):
 			slot = self.caseframe.slots[i]
 
 			# Check if filler legal(given limit, adjustment rule)
-			
-
 			for filler in self.fillers[i]:
-				if not (filler.sem_type.compatible(slot.sem_type)):
+				if not filler.sem_type.compatible(slot.sem_type):
 					raise Exception('Incompatible filler provided for slot:\n' \
-					+ 'Slot has type: ' + slot.sem_type + ' while Filler has type: '\
-					+ filler.type)
+					+ 'Slot has type: ' + slot.sem_type + ', and filler has type: ' + filler.type)
