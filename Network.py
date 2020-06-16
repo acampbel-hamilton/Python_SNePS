a9cb93247daf9171f572db3c52f462aef1978797
swiseman <swiseman@hamilton.edu>
2020-06-16 09:53:22 -0400

    Prepare for new methods on network

--- Network.py
+++ Network.py
@@ -9,31 +9,35 @@ class Network:
         self.slots = {} # AKA Relations
         self.sem_hierarchy = SemanticHierarchy()
         self.contexts = {}
-        self.defaultContext = Context("_default", docstring="The default context", \
-            hyps={}, ders={})
+        self.defaultContext = Context("_default", docstring="The default context", hyps={}, ders={})
 
-    def defineType(self, name, parent_names = []):
+    def defineType(self, name, parent_names=[]):
         # Adds term to hierarchy
         self.sem_hierarchy.add_type(name, parent_names)
 
     def defineTerm(self, name, docstring="", sem_type_name="Entity"):
         # Creates base atomic node
 
-        # Respecification
         if name in self.nodes:
+            # Respecification
             node = self.nodes[name]
             current_type = node.sem_type
             new_type = self.sem_hierarchy[sem_type_name]
             node.sem_type = self.sem_hierarchy.respecification(name, current_type, new_type)
-            return
-
-        # Creation
-        sem_type = self.sem_hierarchy.get_type(sem_type_name)
-        self.nodes[name] = Node.Base(name, sem_type, docstring)
+        else:
+            # Creation
+            sem_type = self.sem_hierarchy.get_type(sem_type_name)
+            self.nodes[name] = Node.Base(name, sem_type, docstring)
 
     def allTerms(self):
         for term in self.nodes:
             print(term)
 
-    def allTypes(self):
+    def showTypes(self):
         print(self.sem_hierarchy)
+
+    def defineCaseframe():
+
+    def defineContext():
+
+    def defineSlot():
