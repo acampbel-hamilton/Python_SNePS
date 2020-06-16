954effcf23ae2b1377556f9440638687ba80b791
Julia Ganson <juliaganson@Julias-MacBook.local>
2020-06-16 10:01:04 -0400

    made names consistent

--- Network.py
+++ Network.py
@@ -9,13 +9,13 @@ class Network:
         self.slots = {} # AKA Relations
         self.sem_hierarchy = SemanticHierarchy()
         self.contexts = {}
-        self.defaultContext = Context("_default", docstring="The default context", hyps={}, ders={})
+        self.default_context = Context("_default", docstring="The default context", hyps={}, ders={})
 
-    def defineType(self, name, parent_names=[]):
+    def define_type(self, name, parent_names=[]):
         # Adds term to hierarchy
         self.sem_hierarchy.add_type(name, parent_names)
 
-    def defineTerm(self, name, docstring="", sem_type_name="Entity"):
+    def define_term(self, name, docstring="", sem_type_name="Entity"):
         # Creates base atomic node
 
         if name in self.nodes:
@@ -29,18 +29,18 @@ class Network:
             sem_type = self.sem_hierarchy.get_type(sem_type_name)
             self.nodes[name] = Node.Base(name, sem_type, docstring)
 
-    def allTerms(self):
+    def all_terms(self):
         for term in self.nodes:
             print(term)
 
-    def showTypes(self):
+    def show_types(self):
         print(self.sem_hierarchy)
 
-    def defineCaseframe():
+    def define_caseframe():
         pass
 
-    def defineContext():
+    def define_context():
         pass
 
-    def defineSlot():
+    def define_slot():
         pass
