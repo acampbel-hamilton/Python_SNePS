a1950456c9580251c302be72c5e5f59fbee93318
John Madigan <jmadigan@hamilton.edu>
2020-06-18 13:59:51 -0400

    Add path parsing

---
+++ PathLex.py
@@ -0,0 +1,38 @@
+from . import ply
+from re import match
+
+keywords = (
+    'converse',
+    'kplus',
+    'kstar',
+    'compose',
+    'or',
+    'and',
+    'irreflexive-restrict'
+)
+
+tokens = (
+    'LParen',
+    'RParen',
+    'SlotName',
+    'ExPoint',
+    'ReverseSlotName',
+    'Comma'
+)
+
+t_LParen  = r'\('
+t_RParen  = r'\)'
+t_SlotName = r'[A-Za-z_][A-Za-z0-9_]*'
+t_ExPoint = r'\!'
+t_ReverseSlot = r'[A-Za-z_][A-Za-z0-9_]*\-'
+t_Comma = r','
+
+def t_error(t):
+    print("Invalid syntax: ", t.value)
+    t.lexer.skip(1)
+
+t_ignore = ' \t\r\n\f\v'
+
+# Build the lexer
+from .ply import lex
+lexer = lex.lex()
