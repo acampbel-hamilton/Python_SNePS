6ecb01dadbf6aec9ba748a9094c83827f03ec6fe
Ben <bkallus@hamilton.edu>
2020-06-23 15:27:36 -0400

    looked at testing

--- test.py
+++
@@ -1,5 +0,0 @@
-from psneps import *
-
-net = Network.Network()
-net.assert_wft("if(a, b)")
-net.assert_wft("=>(a, b)")
--- tests/test_Node.py
+++ tests/test_Node.py
@@ -8,11 +8,11 @@ class TestNodeMethods(unittest.TestCase):
     def test_find_term(self):
         n = Network.Network()
 
-        #checks that it raises an error when there is no node
+        # checks that it raises an error when there is no node
         with self.assertRaises(Node.NodeError):
             n.find_term('John')
 
-        #checks that it returns the node
+        # checks that it returns the node
         n.define_term('John')
         assert(n.find_term('John').name == 'John')
 
@@ -22,15 +22,15 @@ class TestNodeMethods(unittest.TestCase):
         n.define_type('Robot', ['Thing'])
         n.define_type('Cyborg', ['Human', 'Robot'])
 
-        #check to see that John's type is a human
+        # check to see that John's type is a human
         n.define_term('John', 'Human')
         assert(n.find_term('John').sem_type.name == 'Human')
 
-        #if John is a human and a robot then he is a cyborg
+        # if John is a human and a robot then he is a cyborg
         n.define_term('John', 'Robot')
         assert(n.find_term('John').sem_type.name == 'Cyborg')
 
-        #check that it raises a semantic error if it cannot be respecified
+        # check that it raises a semantic error if it cannot be respecified
         n.define_type('Object')
         with self.assertRaises(SemanticType.SemError):
             n.define_term('John', 'Object')
