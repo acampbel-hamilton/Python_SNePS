361906b2064dc85d90ecdf5abd2ca19faa884c16
John Madigan <jmadigan@hamilton.edu>
2020-06-23 10:58:28 -0400

    Create testing structure

---
+++ tests/NodeTests.py
@@ -0,0 +1,12 @@
+import unittest
+
+import psneps
+from psneps import Node, Network
+
+class TestNodeMethods(unittest.TestCase):
+    def test(self):
+        n = Network.Network()
+        assert(0==0)
+
+if __name__ == '__main__':
+    unittest.main()
