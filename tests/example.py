b89a115df66139fc51da0d51e2270f1c89fa162d
swiseman <swiseman@hamilton.edu>
2020-06-25 16:20:08 -0400

    Better example

--- tests/example.py
+++ tests/example.py
@@ -1,6 +1,9 @@
 from psneps import *
 net = Network.Network()
-net.define_caseframe("Has", "Propositional", ["member", "member"])
-net.define_caseframe("Happy", "Propositional", ["member"])
+net.define_slot("agent", "Entity")
+net.define_slot("has", "Entity")
+net.define_slot("happy", "Entity")
+net.define_caseframe("Has", "Propositional", ["agent", "has"])
+net.define_caseframe("Happy", "Propositional", ["happy"])
 net.assert_wft("if(Has(Dog, Bone), Happy(Dog))")
 net.print_graph()
