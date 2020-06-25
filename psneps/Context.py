73ee4e1a3c7a88cf3fd72d33af2f87e0fcb6a423
swiseman <swiseman@hamilton.edu>
2020-06-25 15:08:11 -0400

    RegEx matching

--- psneps/Context.py
+++ psneps/Context.py
@@ -1,4 +1,5 @@
 from .Error import SNError
+from re import match
 
 class ContextError(SNError):
     pass
@@ -40,7 +41,7 @@ class ContextMixin:
         self.default_context = Context("_default", docstring="The default context", hyps={}, ders={})
 
     def define_context(self, name, docstring="", parent="_default", hyps={}, ders={}):
-        
+
         if self.enforce_name_syntax and not match(r'[A-Za-z_][A-Za-z0-9_]*', name):
             raise CaseframeError("ERROR: The context name '{}' is not allowed".format(name))
 
--- psneps/SemanticType.py
+++ psneps/SemanticType.py
@@ -1,5 +1,6 @@
 from math import inf
 from .Error import SNError
+from re import match
 
 class SemError(SNError):
     pass
