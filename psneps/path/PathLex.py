70171b2a7f3aa2eb319b973bbfa9f0656c7c3d8d
swiseman <swiseman@hamilton.edu>
2020-06-29 15:25:11 -0400

    Test for PathLex

--- psneps/path/PathLex.py
+++ psneps/path/PathLex.py
@@ -51,3 +51,22 @@ t_ignore = ' \t\r\n\f\v'
 
 # Build the lexer
 lexer = lex.lex()
+
+if __name__ == '__main__':
+    lexer = lex.lex()
+    while True:
+        try:
+            s = input('Command: ')
+        except EOFError:
+            break
+        if s == 'exit()':
+            break
+        try:
+            lexer.input(s)
+            while True:
+                token = lexer.token()
+                if not token:
+                    break
+                print(token)
+        except:
+            print("Syntax error")
