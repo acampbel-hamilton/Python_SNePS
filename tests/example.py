024ca90d336eb57bfc6530df7fb8f33a419bdb29
swiseman <swiseman@hamilton.edu>
2020-07-03 17:58:20 -0400

    Better example

--- tests/example.py
+++ tests/example.py
@@ -5,10 +5,10 @@ net.define_slot("has", "Entity")
 net.define_slot("happy", "Entity", path="irreflexive-restrict(converse(and(agent, compose(member, class))))", min=0, pos_adj="reduce", neg_adj="expand")
 
 net.define_caseframe("Has", "Propositional", ["agent", "has"])
-net.define_caseframe("Happy", "Propositional", ["happy"])
+# net.define_caseframe("Happy", "Propositional", ["happy"])
 # net.assert_wft("if(Has(Dog, Bone), Happy(Dog))")
 # net.assert_wft("if(not(Has(Dog, Bone)), not(Happy(Dog)))")
-net.assert_wft("4=>(Has(Bone, Bone), Happy(Dog))")
+net.assert_wft("=>([Has(Food, Dog), Has(Dog, Bone), Has(Philosophy, Dog)], Happy(Dog))")
 # net.assert_wft("Equiv([wft7, and(wft6, wft3)])")
 #
 # net.assert_wft("if( every(x, Isa(x, Dog)) , some(y(x), Dog) )")
@@ -19,3 +19,4 @@ net.assert_wft("4=>(Has(Bone, Bone), Happy(Dog))")
 # net.assert_wft("Isa( every(x, Isa(x, Dog)) , some(y(x), Isa(y, Dog)) )")
 
 net.export_graph()
+net.print_graph()
