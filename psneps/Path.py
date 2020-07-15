32a82025e5fcb158699a125d046ba1b83bcdf572
Ben <bkallus@hamilton.edu>
2020-07-15 11:46:37 -0400

    didn't save

--- psneps/Path.py
+++ psneps/Path.py
@@ -50,7 +50,6 @@ class AndPaths(ComposedPaths):
                 first = False
             else:
                 derived.intersection_update(path.derivable(start_node, converse))
-            i += 1
         return derived
 
     def __str__(self) -> str:
