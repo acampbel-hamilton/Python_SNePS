c95b71a3137763f11dcc1b1bfca9e2fbf536c5ae
swiseman <swiseman@hamilton.edu>
2020-07-16 10:56:12 -0400

    Donkey sentence

--- demo/donkey_sentence.py
+++ demo/donkey_sentence.py
@@ -18,10 +18,6 @@ net.define_slot("owns", "Thing")
 net.define_caseframe("Owns", "Proposition", ["owner", "owns"])
 
 net.assert_wft("Beats(every(x, [Farmer(x), Owns(x, some(y(x), Donkey(y)))]), y)")
-net.assert_wft("Beats(every(x, [Farmer(x), Owns(x, some(y(x), Donkey(y)))]), y)")
-
-net.assert_wft("Beats(every(x, [Farmer(x)]), some(y(x), Donkey(y)))")
-net.assert_wft("Beats(every(x, [Farmer(x)]), some(y(x), Donkey(y)))")
 
 net.export_graph()
 net.print_graph()
